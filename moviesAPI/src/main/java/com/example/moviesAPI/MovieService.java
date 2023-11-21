bb1782b4c7fec59aed6c4e7912e51273d7b8f092
Rubal <rubal.goyal@outlook.com>
2023-11-20 17:23:00 -0700

    Added Movie service and controller

---
+++ moviesAPI/src/main/java/com/example/moviesAPI/MovieService.java
@@ -0,0 +1,23 @@
+package com.example.moviesAPI;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired  //Automatically initialize the repository instance
+    private MovieRepository movieRepository; //To interact with MongoDB database
+    public List<Movie> allMovies(){
+        return movieRepository.findAll();
+
+    }
+
+    public Optional<Movie> singleMovie(ObjectId id){
+        return movieRepository.findById(id);
+    }
+}
